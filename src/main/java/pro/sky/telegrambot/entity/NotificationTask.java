e414af89a595611705f7d7080166ac40740db241
BuenosDiasGente <morkovka03121981@gmail.com>
2023-09-09 15:30:33 +0300

    Добавила @ GeneratedValue

--- src/main/java/pro/sky/telegrambot/entity/NotificationTask.java
+++ src/main/java/pro/sky/telegrambot/entity/NotificationTask.java
@@ -2,18 +2,15 @@ package pro.sky.telegrambot.entity;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
 @Entity
 @Table(name = "notification_task")
 public class NotificationTask {
-
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
